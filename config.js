af52d69f69192954bb4b5aa2ea6e42bda5bf4282
Rizkikun <101055276+Rizkikun@users.noreply.github.com>
2022-03-14 13:43:23 +0700

    config.js

--- config.js
+++ config.js
@@ -47,7 +47,7 @@ global.author = '091102'
 global.wait = '「 ⏱️ 」Harap tunggu...'
 global.eror = '「❗」Server ERROR!'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
-global.watermark = '© BOT TZY' //change the watermark 
+global.watermark = '© BOT IKY' //change the watermark 
 global.image = 'https://telegra.ph/file/0bdd84bd5a8099a789828.jpg' //change the image
 global.thumbfoto = 'https://telegra.ph/file/74ee3213700db6534e59a.png'
 

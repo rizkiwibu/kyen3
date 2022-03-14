57be3eda3e59e75b94307a6ea1ad3158fe45f059
Rizkikun <101055276+Rizkikun@users.noreply.github.com>
2022-03-14 13:49:51 +0700

    creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -6,17 +6,17 @@ async function handler(m) {
   let njid = number + '@s.whatsapp.net'
   let onW = await this.isOnWhatsApp(njid) || { isBusiness: false }
 
-  let name2 = 'BOT TZy'
+  let name2 = 'Rizki'
   number2 = owner[1].replace(/[^0-9]/g, '')
   let njid2 = number2 + '@s.whatsapp.net'
   let onW2 = await this.isOnWhatsApp(njid2) || { isBusiness: false }
 
-  let name3 = 'AlyaXzy'
+  let name3 = 'Riski'
   number3 = owner[2].replace(/[^0-9]/g, '')
   let njid3 = number3 + '@s.whatsapp.net'
   let onW3 = await this.isOnWhatsApp(njid3) || { isBusiness: false }
 
-let name4 = 'TioXzy'
+let name4 = 'Rizki'
   number4 = owner[3].replace(/[^0-9]/g, '')
   let njid4 = number4 + '@s.whatsapp.net'
   let onW4 = await this.isOnWhatsApp(njid4) || { isBusiness: false }

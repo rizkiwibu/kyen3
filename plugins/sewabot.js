23af340074df46e77aeb0b15dca0dd5bfcf9d31f
rizkiwibu <98686896+rizkiwibu@users.noreply.github.com>
2022-03-16 12:21:59 +0700

    sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -11,8 +11,8 @@ let handler = async(m, { conn }) => {
 Silahkan klik pada "List Harga" untuk melihat list.
 
 Pembayaran:
-Pulsa : 085730903853
-GoPay: 085730903853
+Dana : 083817161861
+GoPay: 083817161861
 `.trim()
     const button = {
         buttonText: 'List Harga',
